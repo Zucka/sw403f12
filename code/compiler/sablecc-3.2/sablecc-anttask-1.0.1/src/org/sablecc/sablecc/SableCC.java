80126655f08e3a29c2f7deab87c596fc83dd7765
Johan Sørensen <js6@me.com>
2012-03-20 15:50:17 +0100

    Tilføjet rettelser og SableCC ting

---
+++ code/compiler/sablecc-3.2/sablecc-anttask-1.0.1/src/org/sablecc/sablecc/SableCC.java
@@ -0,0 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * This file is part of AntTask.                             *
+ * See the file "LICENSE" for copyright information and the  *
+ * terms and conditions for copying, distribution and        *
+ * modification of AntTask.                                  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package org.sablecc.sablecc;
+
+import java.io.*;
+
+public class SableCC
+{
+  public static void processGrammar(String grammar, String destDir) throws Exception, Throwable 
+  {
+  }
+}
